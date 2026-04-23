049af1f3c7ca7b78b1debf1053b6529f3004378f
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-04-23 23:04:30 +0200

    Update Gracz.h

--- Gracz.h
+++ Gracz.h
@@ -1,15 +1,24 @@
 #pragma once
 #include "Karta.h"
 
+class Kasyno;
+
 class Gracz
 {
 private:
     Karta* reka[10];
     int liczbaKart;
     int sumaPunktow;
+    bool pass;
+    Kasyno* kasyno;
 
 public:
     Gracz();
+
     void wezKarte(Karta* _karta);
     void pokazKarty();
+    void ustawKasyno(Kasyno* _kasyno);
+    void wykonajRuch();
+
+    bool czyPass();
 };

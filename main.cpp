f51f9363337081c647d8dc338496bccb456a5793
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-04-24 12:31:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include "Kasyno.h"
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    Kasyno kasyno;
+    int liczbaGraczy;
+
+    cout << "Podaj liczbe graczy (1-3): ";
+    cin >> liczbaGraczy;
+
+    while (liczbaGraczy < 1 or liczbaGraczy > 3)
+    {
+        cout << "Bledna liczba. Podaj jeszcze raz (1-3): ";
+        cin >> liczbaGraczy;
+    }
+
+    Kasyno kasyno(liczbaGraczy);
     kasyno.graj();
 
     return 0;

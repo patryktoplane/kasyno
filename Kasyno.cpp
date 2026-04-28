91bfbf0e4e3b64251695bd3bcf93e036eef8f7a2
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-04-28 21:11:49 +0200

    Update Kasyno.cpp

--- Kasyno.cpp
+++ Kasyno.cpp
@@ -1,12 +1,18 @@
 #include "Kasyno.h"
+#include <iostream>
 #include <time.h>
+#include <cstdlib>
 using namespace std;
 
-Kasyno::Kasyno(int _liczbaGraczy)
+Kasyno::Kasyno(int _liczbaGraczy, int _liczbaBotow)
 {
     aktualnaKarta = 0;
+
     liczbaGraczy = _liczbaGraczy;
+    liczbaBotow = _liczbaBotow;
+
     gracze = new Gracz[liczbaGraczy];
+    boty = new Bot[liczbaBotow];
 
     int licznik = 0;
 
@@ -24,20 +30,53 @@ Kasyno::Kasyno(int _liczbaGraczy)
         gracze[i].ustawKasyno(this);
     }
 
+    for (int i = 0; i < liczbaBotow; i++)
+    {
+        boty[i].ustawKasyno(this);
+    }
+
+    for (int i = 0; i < liczbaBotow; i++)
+    {
+        if (i == 0)
+        {
+            boty[i].ustawBota(19, rand() % 3);
+        }
+        else if (i == 1)
+        {
+            boty[i].ustawBota(17, rand() % 3);
+        }
+        else
+        {
+            boty[i].ustawBota(15, rand() % 3);
+        }
+    }
+
     tasujKarty();
 }
 
 Kasyno::~Kasyno()
 {
     delete[] gracze;
+    delete[] boty;
 }
 
 void Kasyno::graj()
 {
+    resetujGre();
+
+    cout << endl;
+    cout << "Rozdanie dwoch pierwszych kart." << endl;
+
     for (int i = 0; i < liczbaGraczy; i++)
     {
         gracze[i].wezKarte(dajKarte());
         gracze[i].wezKarte(dajKarte());
+    }
+
+    for (int i = 0; i < liczbaBotow; i++)
+    {
+        boty[i].wezKarte(dajKarte());
+        boty[i].wezKarte(dajKarte());
     }
 
     while (!czyWszyscySpasowali())
@@ -51,6 +90,16 @@ void Kasyno::graj()
                 gracze[i].wykonajRuch();
             }
         }
+
+        for (int i = 0; i < liczbaBotow; i++)
+        {
+            if (!boty[i].czyPass())
+            {
+                cout << endl;
+                cout << "Ruch bota " << i + 1 << endl;
+                boty[i].wykonajRuch();
+            }
+        }
     }
 
     cout << endl << "KONIEC GRY" << endl << endl;
@@ -61,6 +110,15 @@ void Kasyno::graj()
         gracze[i].pokazKarty();
         cout << endl;
     }
+
+    for (int i = 0; i < liczbaBotow; i++)
+    {
+        cout << "Bot " << i + 1 << ":" << endl;
+        boty[i].pokazKarty();
+        cout << endl;
+    }
+
+    pokazWyniki();
 }
 
 void Kasyno::tasujKarty()
@@ -100,5 +158,82 @@ bool Kasyno::czyWszyscySpasowali()
         }
     }
 
+    for (int i = 0; i < liczbaBotow; i++)
+    {
+        if (!boty[i].czyPass())
+        {
+            return false;
+        }
+    }
+
     return true;
+}
+
+void Kasyno::pokazWyniki()
+{
+    int najlepszyWynik = -1;
+
+    for (int i = 0; i < liczbaGraczy; i++)
+    {
+        if (gracze[i].getSumaPunktow() <= 21)
+        {
+            if (gracze[i].getSumaPunktow() > najlepszyWynik)
+            {
+                najlepszyWynik = gracze[i].getSumaPunktow();
+            }
+        }
+    }
+
+    for (int i = 0; i < liczbaBotow; i++)
+    {
+        if (boty[i].getSumaPunktow() <= 21)
+        {
+            if (boty[i].getSumaPunktow() > najlepszyWynik)
+            {
+                najlepszyWynik = boty[i].getSumaPunktow();
+            }
+        }
+    }
+
+    if (najlepszyWynik == -1)
+    {
+        cout << endl << "Wszyscy przekroczyli 21 punktow. Brak zwyciezcy." << endl;
+        return;
+    }
+
+    cout << endl;
+    cout << "Najlepszy wynik: " << najlepszyWynik << endl;
+    cout << "Wygrywa:" << endl;
+
+    for (int i = 0; i < liczbaGraczy; i++)
+    {
+        if (gracze[i].getSumaPunktow() == najlepszyWynik)
+        {
+            cout << "Gracz " << i + 1 << endl;
+        }
+    }
+
+    for (int i = 0; i < liczbaBotow; i++)
+    {
+        if (boty[i].getSumaPunktow() == najlepszyWynik)
+        {
+            cout << "Bot " << i + 1 << endl;
+        }
+    }
+}
+
+void Kasyno::resetujGre()
+{
+    aktualnaKarta = 0;
+    tasujKarty();
+
+    for (int i = 0; i < liczbaGraczy; i++)
+    {
+        gracze[i].resetujGracza();
+    }
+
+    for (int i = 0; i < liczbaBotow; i++)
+    {
+        boty[i].resetujGracza();
+    }
 }

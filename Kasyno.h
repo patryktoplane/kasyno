ba80e9795c7c3e6654579d25142e37713a37b28f
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-04-28 21:11:40 +0200

    Update Kasyno.h

--- Kasyno.h
+++ Kasyno.h
@@ -1,20 +1,30 @@
 #pragma once
 #include "Karta.h"
 #include "Gracz.h"
+#include "Bot.h"
 
 class Kasyno
 {
 private:
     Karta talia[52];
-    int aktualnaKarta;
+
     Gracz* gracze;
+    Bot* boty;
+
+    int aktualnaKarta;
     int liczbaGraczy;
+    int liczbaBotow;
 
 public:
-    Kasyno(int liczbaGraczy);
+    Kasyno(int _liczbaGraczy, int _liczbaBotow);
     ~Kasyno();
+
     Karta* dajKarte();
+
     void graj();
     void tasujKarty();
+    void pokazWyniki();
+    void resetujGre();
+
     bool czyWszyscySpasowali();
 };

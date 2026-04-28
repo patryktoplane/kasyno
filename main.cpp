687d2325fcc3c65c3773175a5a45ac517c08f34d
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-04-28 21:11:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,18 +5,42 @@ using namespace std;
 int main()
 {
     int liczbaGraczy;
+    int liczbaBotow;
+    int wybor;
 
-    cout << "Podaj liczbe graczy (1-3): ";
+    cout << "Podaj liczbe graczy: ";
     cin >> liczbaGraczy;
 
-    while (liczbaGraczy < 1 or liczbaGraczy > 3)
+    while (liczbaGraczy < 1 || liczbaGraczy > 3)
     {
-        cout << "Bledna liczba. Podaj jeszcze raz (1-3): ";
+        cout << "Bledna liczba. Podaj liczbe od 1 do 3: ";
         cin >> liczbaGraczy;
     }
 
-    Kasyno kasyno(liczbaGraczy);
-    kasyno.graj();
+    cout << "Podaj liczbe botow: ";
+    cin >> liczbaBotow;
+
+    while (liczbaBotow < 1 || liczbaBotow > 3)
+    {
+        cout << "Bledna liczba. Podaj liczbe od 1 do 3: ";
+        cin >> liczbaBotow;
+    }
+
+    Kasyno kasyno(liczbaGraczy, liczbaBotow);
+
+    do
+    {
+        kasyno.graj();
+
+        cout << endl;
+        cout << "Czy chcesz rozpoczac nowa gre?" << endl;
+        cout << "1: Tak" << endl;
+        cout << "0: Nie" << endl;
+        cin >> wybor;
+
+    } while (wybor == 1);
+
+    cout << "Koniec programu." << endl;
 
     return 0;
 }
